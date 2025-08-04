6aa13b47199eae02091eab524af9ca05669cc42a
I-Amm-Vaibhav <vaibhavpatnaik@gmail.com>
2025-08-04 21:01:04 +0530

    Working version with User entity and POST implemented

--- ParcelMgmt/src/main/java/com/parcelmgmt/ParcelMgmt/model/User.java
+++ ParcelMgmt/src/main/java/com/parcelmgmt/ParcelMgmt/model/User.java
@@ -5,9 +5,6 @@ import lombok.*;
 
 @Entity
 @Table(name = "users")  // Avoids conflict with SQL keyword "user"
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
 
     @Id
@@ -21,4 +18,82 @@ public class User {
     private String address;
     private String password;
     private String preference;
+
+    public User(Long userId, String role, String name, String email, Long phNo, String address, String password, String preference) {
+        this.userId = userId;
+        this.role = role;
+        this.name = name;
+        this.email = email;
+        this.phNo = phNo;
+        this.address = address;
+        this.password = password;
+        this.preference = preference;
+    }
+    public User(){
+
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getPhNo() {
+        return phNo;
+    }
+
+    public void setPhNo(Long phNo) {
+        this.phNo = phNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPreference() {
+        return preference;
+    }
+
+    public void setPreference(String preference) {
+        this.preference = preference;
+    }
 }
